3d3b453360069d090da4ad8672989ac4c16ee47a
Rebecca Garner <rebecca.garner@mail.concordia.ca>
2024-10-17 22:20:06 -0700

    Cleaned 10-ela_phytoplankton_species.R

--- scripts/10-ela_phytoplankton_species.R
+++ scripts/10-ela_phytoplankton_species.R
@@ -1,9 +1,5 @@
 # ELA phytoplankton species cell count, density, and biomass data
 
-setwd("C:/Users/Gandalf/Dropbox/projects/ela18s/")
-setwd("~/Dropbox/projects/ela18s/")
-setwd("~/Library/CloudStorage/Dropbox-Personal/projects/ela18s/")
-
 # Load libraries
 library(tidyverse)
 library(lubridate)
